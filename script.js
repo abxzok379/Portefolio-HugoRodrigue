4868845a3e364997cf9e4e6f14e3cf68fe8aa977
Nicolas Cuffia-Bergeron <129567055+abxzok379@users.noreply.github.com>
2023-09-18 00:21:57 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-function myFunction(){
-    const menu = document.querySelector('#menu');
+function openMenu() {
+    var menu = document.querySelector('#menu');
+    menu.classList.remove('close');
     menu.classList.toggle('show');
-}
+  }
+  
+  function closeMenu() {
+    var menu = document.querySelector('#menu');
+    menu.classList.remove('show');
+    menu.classList.toggle('close');
+  }
+  
+  function nextPage() {
+    var menu = document.querySelector('#menu');
+    var page = document.querySelector('#page2');
+    menu.classList.remove('show');
+    menu.classList.toggle('close');
+    page.classList.toggle('show');
+  }
