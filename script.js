4f685498bb1c633bbdedcf66fbac6520402fdd7d
Nicolas Cuffia-Bergeron <129567055+abxzok379@users.noreply.github.com>
2023-10-04 18:59:28 -0400

    Ajout script curseur perso

--- script.js
+++ script.js
@@ -17,3 +17,18 @@ function openMenu() {
     menu.classList.toggle('close');
     page.classList.toggle('show');
   }
+
+   // Curseur // 
+
+   document.addEventListener('DOMContentLoaded', function(){
+    let cursor = document.querySelector('.custom-cursor');
+    let cursorBefore = document.querySelector('.custom-cursor-before');
+
+    document.addEventListener('mousemove', function(e){
+       cursor.style.left = e.clientX+'px';
+       cursor.style.top = e.clientY+'px';
+       cursorBefore.style.left = e.clientX+'px';
+       cursorBefore.style.top = e.clientY+'px';
+
+    })
+  })
