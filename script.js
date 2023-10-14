4f309b949de170cb703a52771554f76dec10e5ea
Nicolas Cuffia-Bergeron <129567055+abxzok379@users.noreply.github.com>
2023-10-14 00:36:33 -0400

    Clean code

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-
-
-
-
 function openMenu() {
     var menu = document.querySelector('#menu');
     menu.classList.remove('close');
     menu.classList.toggle('show');
   }
   
-  function closeMenu() {
+function closeMenu() {
     var menu = document.querySelector('#menu');
     menu.classList.remove('show');
     menu.classList.toggle('close');
   }
   
-  function nextPage() {
+function nextPage() {
     var menu = document.querySelector('#menu');
     var page = document.querySelector('#page2');
     menu.classList.remove('show');
@@ -22,18 +18,15 @@ function openMenu() {
     page.classList.toggle('show');
   }
 
-   // Curseur // 
-
    document.addEventListener('DOMContentLoaded', function(){
     let cursor = document.querySelector('.custom-cursor');
     let cursorBefore = document.querySelector('.custom-cursor-before');
 
-    document.addEventListener('mousemove', function(e){
+   document.addEventListener('mousemove', function(e){
        cursor.style.left = e.clientX+'px';
        cursor.style.top = e.clientY+'px';
        cursorBefore.style.left = e.clientX+'px';
        cursorBefore.style.top = e.clientY+'px';
-
     })
   })
 
